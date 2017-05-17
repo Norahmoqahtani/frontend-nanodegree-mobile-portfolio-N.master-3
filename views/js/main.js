96b9e1446e9fcf014d31a529dc2379aecc43da7f
Norahmoqahtani <norah-qahtani@hotmail.com>
2017-05-17 23:39:11 +0300

    Update

--- views/js/main.js
+++ views/js/main.js
@@ -504,9 +504,9 @@ function updatePositions() {
     window.performance.mark("mark_start_frame");
     
     var items = document.querySelectorAll('.mover');
-    var number = (document.body.scrollTop / 1250);
+    var scrollTop = (document.body.scrollTop / 1250);
     for (var i = 0; i < items.length; i++) {
-        var phase = Math.sin(number + (i % 5));
+        var phase = Math.sin(scrollTop + (i % 5));
         
         items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
     }
@@ -528,7 +528,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function () {
     var cols = 8;
     var s = 256;
-    for (var i = 0; i < 20; i++) {
+    for (var i = 0; i < 44; i++) {
         var elem = document.createElement('img');
         elem.className = 'mover';
         elem.src = "images/pizza.png";
